3d8d61819551d5e255399e1ea299498eefcc3938
Winnard Arthur <winnardarthur7@gmail.com>
2023-08-31 20:45:59 +0000

    feat: minor changes and display chats

--- app/hooks/useOtherUser.ts
+++ app/hooks/useOtherUser.ts
@@ -10,18 +10,18 @@ const useOtherUser = (
 ) => {
   const session = useSession();
 
-  const otherUsers = useMemo(() => {
+  const otherUser = useMemo(() => {
     const currentUserEmail = session?.data?.user?.email;
 
     //   Filter current user
-    const otherUser = conversation.users.filter(
+    const otherUsers = conversation.users.filter(
       (user) => user.email !== currentUserEmail
     );
 
-    return otherUser;
+    return otherUsers[0];
   }, [session?.data?.user?.email, conversation.users]);
 
-  return otherUsers;
+  return otherUser;
 };
 
 export default useOtherUser;

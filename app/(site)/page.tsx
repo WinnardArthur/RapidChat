2eb014ab508117787aa00c74d19e335b242c57ef
Winnard Arthur <winnardarthur7@gmail.com>
2023-09-12 08:51:06 +0000

    feat: add logo

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,14 +5,13 @@ export default function Home() {
   return (
     <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        RapidChat
-        {/* <Image
+        <Image
           alt='RapidChat'
-          height='48'
-          width='48'
+          height='80'
+          width='80'
           className="mx-auto w-auto"
           src='/images/logo.png'
-        /> */}
+        />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
           Sign in to your account
         </h2>
